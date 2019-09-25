f98fddeb41586a8b9ea2352d2ef2bc030c3c08ed
ybpei <513975171@qq.com>
2019-09-25 19:43:21 +0800

    second test modify

--- classifier.py
+++ classifier.py
@@ -4,4 +4,5 @@ import torch
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
-        self.relu = torch.ReLU()
+        self.relu = nn.ReLU()
+        self.conv1 = nn.conv2d(3, 32, 3, 1)
